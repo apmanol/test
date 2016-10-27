f96352ff60810b1902cc5ad8509937551f57b68d
Apostolos Manolitzas <manap@intracom-telecom.com>
2016-10-27 22:05:03 +0300

    Attempt #15

--- hello-cpp-world.cc
+++ hello-cpp-world.cc
@@ -6,26 +6,21 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
-
-TEST_CASE( "Threads are spawned", "[threadorial]" ) {
-    // REQUIRE( Factorial(1) == 1 );
-    // REQUIRE( Factorial(2) == 2 );
-    // REQUIRE( Factorial(3) == 6 );
-    // REQUIRE( Factorial(10) == 3628800 );
-    std::vector<std::thread> threads;
-    for (auto i =0 ; i < 10; i++)
-    {
-        std::cout << "starting threads" << std::endl;
-        threads.push_back(std::thread([i](){
-            std::cout << "starting thread" << i << std::endl;
+TEST_CASE("Threads are spawned", "[threadorial]") {
+  // REQUIRE( Factorial(1) == 1 );
+  // REQUIRE( Factorial(2) == 2 );
+  // REQUIRE( Factorial(3) == 6 );
+  // REQUIRE( Factorial(10) == 3628800 );
+  std::vector<std::thread> threads;
+  for (auto i = 0; i < 10; i++) {
+    std::cout << "starting threads" << std::endl;
+    threads.push_back(std::thread(
+        [i]() { // std::cout << "starting thread" << i << std::endl;
         }));
-    }
+  }
 
+  std::for_each(threads.begin(), threads.end(),
+                [](std::thread &t) { t.join(); });
 
-    std::for_each(threads.begin(),threads.end(), [](std::thread &t){ 
-        t.join();
-    });
-    
-    auto k = new char[100];
-
+  auto k = new char[100];
 }
